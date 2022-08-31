9fa2c798e6d3d155ad8d8d3d73aebec9389f210a
raja <rvnasaka23>
2022-08-31 11:38:16 +0530

    removed unnecessary files, add when necessary

--- src/main/java/com/example/movie/Bomysh/repository/TheatreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.movie.Bomysh.repository;
-
-import com.example.movie.Bomysh.domain.Theatre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheatreRepository extends JpaRepository<Theatre,Integer> {
-}
